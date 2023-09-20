9e8c4b2a0798d5222401b8ff5485fba248e9f071
Jacek Skoczek <jaca1989@gmail.com>
2023-09-20 09:26:55 +0200

    static files delete

--- Ekstrastats/settings.py
+++ Ekstrastats/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-oln*+5y#v9($4cmwvzhd7r9g@i)1niv4myxe@whb(og3vza1i!'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
