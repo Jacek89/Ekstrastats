3c41ba1c8014bedd211d6fca0feb28c0e9f98c07
Jacek Skoczek <jaca1989@gmail.com>
2023-10-10 17:24:35 +0200

    crispy form customization added

--- Ekstrastats/settings.py
+++ Ekstrastats/settings.py
@@ -40,7 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'whitenoise.runserver_nostatic'
+    'whitenoise.runserver_nostatic',
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
 
 MIDDLEWARE = [
@@ -132,3 +134,9 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Crispy Forms
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 from datetime import date
-
-
-
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit, Div
+from crispy_forms.bootstrap import PrependedText
 
 class TableDate(forms.Form):
     date_from = forms.DateField(label='from:',
@@ -12,3 +12,17 @@ class TableDate(forms.Form):
     date_to = forms.DateField(label='to:',
                               initial=date.today,
                               widget=forms.widgets.DateInput(attrs={'type': 'date'}))
+
+    def __init__(self, *args, **kwargs):
+        super(TableDate, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_id = 'table-form'
+        self.helper.form_show_labels = False
+        self.helper.layout = Layout(
+            Div(
+                Div(PrependedText('date_from', "from:"), css_class='col-md-5'),
+                Div(PrependedText('date_to', "to:"), css_class='col-md-5'),
+                Div(Submit('submit', 'Filter'), css_class='col-md-2'),
+                css_class='row pt-3',
+                )
+        )
