51861ba77885980b28ec2461c52d94c0f97192b4
Jacek Skoczek <jaca1989@gmail.com>
2023-10-13 18:51:19 +0200

    Another Security upgrade (with low SECURE_HSTS_SECONDS)

--- Ekstrastats/settings/settings.py
+++ Ekstrastats/settings/settings.py
@@ -150,9 +150,16 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 # Security:
 
-SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'Strict'
+SESSION_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
 SECURE_SSL_REDIRECT = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_HSTS_SECONDS = 300  # set low, but when site is ready for deployment, set to at least 15768000 (6 months)
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
 
 # Load local settings if exists
 
